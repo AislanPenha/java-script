2f8eccc764188c76232843730fa5d15d4f66de5a
Aislan Penha <130594608+AislanPenha@users.noreply.github.com>
2025-06-14 08:06:17 -0300

    Update index.js

--- 05 - Objetos e prototypes/aula-04/index.js
+++ 05 - Objetos e prototypes/aula-04/index.js
@@ -1,3 +1,17 @@
+/*
+Javascript é baseado em protótipos para passar propriedades e métodos 
+de um objeto para outro.
+
+Definição de protótipo
+É o termo usado apra se referir ao que foi criado pela
+primeira vez, servindo de modelo ou molde para futuras produções.
+
+Todos os objetos tem uma referência interna para um protótipo (__proto__)
+que vem da propriedade prototype da função construtora que foi usada para
+criá-la. Quando tentamos acessar um membro de um objeto, primeiro o motor
+do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia
+de prótotipos é usada até o topo (null) até encontrar (ou não) tal membro.
+*/
 function Pessoa(nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
@@ -5,7 +19,7 @@ function Pessoa(nome, sobrenome){
     this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
 }
 Pessoa.prototype.parametro = 'Olha aí';
-
+Pessoa.prototype = p1.__proto__
 Pessoa.prototype.nomeFull = function(){        // Se tiver o mesmo nome de algum método construtor o do construtor terá prioridade
     return this.nome + ' ' + this.sobrenome;
 }
